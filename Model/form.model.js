41522a78743a308b31c616e37a3c814e59a587ed
raghavagarwal701 <95160038+raghavagarwal701@users.noreply.github.com>
2023-08-31 00:44:55 +0530

    Update form.model.js

--- Model/form.model.js
+++ Model/form.model.js
@@ -1,16 +1,17 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const Schema=mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const formSchema=new Schema({
-    name:{type:String,required:true},
-    companyName:{type:String,required:true},
-    phoneNumber:{type:String,required:true},
-    email:{type:String,required:true},
-},{
-    timestamps:true,
-})
+const formSchema = new Schema({
+    name: { type: String, required: true },
+    companyName: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String, required: true },
+    userResponses: { type: Schema.Types.Mixed,required:true } // Adding userResponses property
+}, {
+    timestamps: true,
+});
 
-const form=mongoose.model('Form',formSchema);
+const form = mongoose.model('Form', formSchema);
 
-module.exports=form;
+module.exports = form;
