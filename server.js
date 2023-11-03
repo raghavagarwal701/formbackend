1e2dbeccbd8cfce0298fbb87f3f24d7405658adb
raghavagarwal701 <raghavagarwal701@gmail.com>
2023-11-03 21:54:55 +0530

    allowed cors requests

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const formRouter=require('./Component/form');
 require('dotenv').config();
 
 const app = express();
+const corsOptions = {
+    origin: '*',
+};
+  
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
